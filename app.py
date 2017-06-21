a3792a1752c9c32ddd8e1af0b40d454be424eef1
claudio-Pico <claudio-pico@hotmail.com>
2017-06-21 18:23:22 -0300

    borrar init

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+OPENSHIFT_REPO_DIR = os.environ['OPENSHIFT_REPO_DIR']
+os.chdir(OPENSHIFT_REPO_DIR)
+OPENSHIFT_PYTHON_IP = os.environ['OPENSHIFT_PYTHON_IP']
+OPENSHIFT_PYTHON_PORT = os.environ['OPENSHIFT_PYTHON_PORT']
+OPENSHIFT_PYTHON_DIR = os.environ['OPENSHIFT_PYTHON_DIR']
+SERVER_ROOT = os.path.join(OPENSHIFT_PYTHON_DIR, 'run', 'mod_wsgi')
+VIRTUAL_ENV = os.environ['VIRTUAL_ENV']
+program = os.path.join(VIRTUAL_ENV, 'bin', 'mod_wsgi-express')
+os.execl(program, program, 'start-server', 'wsgi.py',
+        '--server-root', SERVER_ROOT, '--log-to-terminal',
+        '--host', OPENSHIFT_PYTHON_IP, '--port', OPENSHIFT_PYTHON_PORT)
