887f30affc61d507413eb8dc57e6728260106cc6
claudio-Pico <claudio-pico@hotmail.com>
2017-06-21 20:20:27 -0300

    borrar init

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     ip = os.environ.get('OPENSHIFT_PYTHON_IP', constantes.ipadrres)
     port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
     # Para detalhae melhor os Logs no OpenShift
-    application.config['PROPAGATE_EXCEPTIONS'] = True
+    #application.config['PROPAGATE_EXCEPTIONS'] = True
 
     application.run(ip,port)
